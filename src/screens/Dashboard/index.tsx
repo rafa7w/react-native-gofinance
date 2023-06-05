c4d96ca219eec7c25c1deee0e205c14f6a393fe6
Rafael William <rafael.fim@hotmail.com>
2023-06-05 19:24:29 -0300

    header dashboard page

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,20 @@
-import { Container } from "./styles";
+import { Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper, Icon } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://github.com/rafa7w.png'}}/>
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Rafael</UserName>
+            </User>
+          </UserInfo>
+          <Icon name='power'/>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
