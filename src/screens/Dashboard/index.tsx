48e6db19aba3178a5946f65b740359a7e9fcbdf4
Rafael William <rafael.fim@hotmail.com>
2023-06-06 00:45:27 -0300

    transaction card render

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper, Icon, HighlightCards, Transactions, Title } from "./styles";
+
 import { HighlightCard } from "@components/Loading/HighlightCard";
-import { Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper, Icon, HighlightCards } from "./styles";
+import { TransactionCard } from "@components/TransactionCard";
+
 
 export function Dashboard() {
   return (
@@ -22,6 +25,11 @@ export function Dashboard() {
         <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 13 de abril"/>
         <HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransaction="01 a 16 de abril"/>
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard />
+      </Transactions>
     </Container>
   )
 }
